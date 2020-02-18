96e82cd6397b164801524e75dcaa05decfe46df0
Bob Saris <bob@bobsaris.com>
2020-02-17 20:17:02 -0500

    Added website code.

---
+++ config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascript_dir = "js"
+fonts_dir = "fonts"
+relative_assets = true
+line_comments = false
+
+asset_cache_buster :none
